8e103be95d3a878ea8bd5fc8792c00cf79f8868d
Erik L. Arneson <earneson@arnesonium.com>
2019-08-19 15:26:00 -0700

    Add emergency user

--- site-modules/profile/manifests/base.pp
+++ site-modules/profile/manifests/base.pp
@@ -9,4 +9,15 @@ class profile::base {
     managehome => false,
     locked => true,
   }
+
+  accounts::user { 'emergency':
+    ensure => present,
+    uid => '460',
+    gid => '460',
+    shell => '/bin/bash',
+    password => '!!', # No password
+    sshkeys => [
+      "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAxXShJiI4H+QnSOsRO+3NTU2X3Xn0g/2qvztPn/kwmKLgwfEtoYybbgpCRWMB5NZgnYh+QbSxvqII1QizVcZJcmprEyESeTHRmT2LGBwSxsSnI3CI3l4FGnSNg2cO6+6XRNcS7fjFxa1DgES6LiXekIGxjcJci1kBMQoBS0NzgTspsUEt2dzWrssEnT6ZZjdVNyMYYOVGfuYvw4My6xgMvzz8amJi+/J6Q9FS18N+46s1qjL/zjNxpxZOSmjad3nHIZZb8I2wtcILFZjMXqyf2rbJt2SYpH43CxajEPMwgYjp2xgKp7xbbOO35mCIswVxPq/lzjh5mlmdW8OTUbqOmw== erik",
+    ],
+  }
 }
