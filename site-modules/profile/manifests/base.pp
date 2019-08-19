1002e8106b4d07b5e8e63d70e641f611aaed4f58
Erik L. Arneson <earneson@arnesonium.com>
2019-08-19 15:01:56 -0700

    Smart quotes are lovely

--- site-modules/profile/manifests/base.pp
+++ site-modules/profile/manifests/base.pp
@@ -2,7 +2,7 @@ class profile::base {
 
   #the base profile should include component modules that will be on all nodes
 
-  accounts::user { ‘monitoring’:
+  accounts::user { 'monitoring':
     managehome => false,
   }
 
