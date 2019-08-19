7b3fae7cd9ea6e0d484100b5a4096c7e4ce276fa
Erik L. Arneson <earneson@arnesonium.com>
2019-08-19 15:09:40 -0700

    More changes to the monitoring user

--- site-modules/profile/manifests/base.pp
+++ site-modules/profile/manifests/base.pp
@@ -3,7 +3,10 @@ class profile::base {
   #the base profile should include component modules that will be on all nodes
 
   accounts::user { 'monitoring':
+    ensure => present,
+    uid => '450',
+    gid => '450',
     managehome => false,
+    locked => true,
   }
-
 }
