2e1ecd9f9a798756361d6a7bd86a3feaa416d1d5
Erik L. Arneson <earneson@arnesonium.com>
2019-08-19 17:06:01 -0700

    Maybe use MariaDB instead
    This is gonna be tricky.

--- site-modules/profile/manifests/mysql.pp
+++ site-modules/profile/manifests/mysql.pp
@@ -1,3 +1,15 @@
 class profile::mysql {
-  include '::mysql::server'
+  class {'::mysql::server':
+    package_name => 'mariadb-server',
+    service_name => 'mariadb',
+    override_options => {
+      mysqld => {
+        'log-error' => '/var/log/mysql/mariadb.log',
+        'pid-file'  => '/var/run/mysqld/mysqld.pid',
+      },
+      mysqld_safe => {
+        'log-error' => '/var/log/mysql/mariadb.log',
+      },
+    }
+  }
 }
